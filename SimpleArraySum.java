c15e5b1affb78bd95385885c85b1dbb9f214ca79
Madhur Agarwal <madhur1812agarwal@gmail.com>
2023-08-16 20:01:01 +0530

    Simple Array Sum

---
+++ SimpleArraySum.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class SimpleArraySum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    static class Result {
+
+        /*
+         * Complete the 'simpleArraySum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY ar as parameter.
+         */
+
+        public static int simpleArraySum(List<Integer> ar) {
+            // Write your code here
+            int sum = 0;
+            for (Integer integer : ar) {
+                sum += integer;
+            }
+
+            return sum;
+
+        }
+
+    }
+}
